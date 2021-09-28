c37781201ff41ff5f96ec742b06b43af1323c6c9
Fabio Medeiros <fabioeomedeiros@gmail.com>
2021-09-28 00:28:01 -0300

    add

--- 01_titanic.py
+++ 01_titanic.py
@@ -1,7 +1,25 @@
 import pandas as pd
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 train = pd.read_csv('datas/train.csv')
 test = pd.read_csv('datas/test.csv')
 
-print(test['Embarked'].value_counts())
+modelo = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state=0)
+
+def transformar_sexo(valor):
+    if valor == 'female':
+        return 1
+    else:
+        return 0
+
+train['Sex_bin'] = train['Sex'].map(transformar_sexo)
+
+variaveis = ['Sex_bin', 'Age']
+
+X = train[variaveis]
+Y = train['Survived']
+
+print(X)
+print(Y)
+
